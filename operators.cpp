5adb2208e7877be2485cb7d962ac704d148b46bb
Tushar <tusharchaudhary4u08@gmail.com>
2025-11-09 00:38:11 +0530

    Modify

--- operators.cpp
+++ operators.cpp
@@ -32,9 +32,9 @@ int main() {
 
     // Logical Operators
     cout<<"Following are the Logical operators in C++:"<<endl;
-    cout<<"The value of this logical 'and operator' ((a==b) && (a<b)) is: "<<((a==b) && (a<b))<<endl;  //In and operator both the conditions must be true to return true(1) or vice versa
-    cout<<"The value of this logical 'or operator' ((a==b) || (a<b)) is: "<<((a==b) || (a<b))<<endl;   //In or operator if any one condition is true it returns true(1) or vice versa
-    cout<<"The value of this logical 'not operator' (!(a==b)) is: "<<(!(a==b))<<endl;                  //In not operator it reverses the condition i.e if the condition is true it returns false(0) or vice versa
+    cout<<"The value of this logical 'and operator' ((a==b) && (a<b)) is: "<<((a==b) && (a<b))<<endl;  //In 'and' operator both the conditions must be true to return true(1) or vice versa
+    cout<<"The value of this logical 'or operator' ((a==b) || (a<b)) is: "<<((a==b) || (a<b))<<endl;   //In 'or' operator if any one condition is true it returns true(1) or vice versa
+    cout<<"The value of this logical 'not operator' (!(a==b)) is: "<<(!(a==b))<<endl;                  //In 'not' operator it reverses the condition i.e if the condition is true it returns false(0) or vice versa
 
 
 }
