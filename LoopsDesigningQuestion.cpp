f851ae16494cac766c9e202d8b2e8d1ac5ab886c
Tushar <tusharchaudhary4u08@gmail.com>
2025-11-11 00:07:12 +0530

    Just One more day of C++

---
+++ LoopsDesigningQuestion.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    // Example 1. to print a 5x5 star pattern using nested loops
+    //     // Rows
+    // for(int i=0; i<5; i++){
+    //     // Columns 
+    //     for(int j=0; j<5; j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+    // Example 2. to print a right-angled triangle star pattern using nested loops
+    // for(int row=0; row<5; row++){
+    //     for(int col=0; col<=row; col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl; 
+    // }
+
+
+    // Example 3. to print an inverted right-angled triangle star pattern using nested loops
+    // for(int row=0; row<5; row++){
+    //     for(int col=5; col>row; col--){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    //        **OR**
+
+    //     for(int i=0; i<5; i++){
+    //     for(int j=0; j<5-i; j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // } 
+
+    
+
+
+
+    // Example 4. to print numbers from 1 to 5 in a right-angled triangle using nested loops
+    // for(int row=1; row<=5; row++){
+    //     for(int col=1; col<=row; col++)
+    //     {
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // Example 5. to print a diamond star pattern using nested loops
+    // for(int row=0; row<5; row++){
+    //     for(int col=0; col<=row; col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // } 
+    // for(int row=0; row<5; row++){
+    //     for(int col=4; col>row; col--){
+    //         cout<<"* ";
+    //         }
+    //         cout<<endl;
+    // }
+
+    // Example 6. to print 5 to 1 in a 5x5 grid using nested loops
+    // for(int i=0; i<5; i++){
+    //     for(int j=5; j>0; j--){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+    // Example 7. to print a pattern using nested loops
+    // for(int i=0; i<5; i++){
+    //     for(int j=5; j>i; j--){
+    //         cout<<j;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // Example 8. to print a pattern using nested loops
+    //     for(int i=0; i<5; i++){
+    //     for(int j=5; j>i-1; j--){
+    //         cout<<j+1<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // Example 9. to print a pattern using nested loops
+    // for (int i = 0; i < 5; i++){
+    //     for (int j = 0; j < 5 - i; j++)
+    //     {
+    //         cout << j + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // Example 10. to print 1 to 5 in a 5x5 grid using nested loops
+    // // Rows
+    // for(int i=0; i<5; i++){
+    //     // Columns
+    //     for(int j=0; j<=i; j++){
+    //         cout<<j+1<<" ";
+    //     }
+    //     cout<<endl; 
+    // }
+
+
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,2 @@
-;
-    cout<<marks[1]<<endl;
-    cout<<marks[2]<<endl;
-    cout<<marks[3]<<endl;
+
+            cout<<j+1<<" ";
