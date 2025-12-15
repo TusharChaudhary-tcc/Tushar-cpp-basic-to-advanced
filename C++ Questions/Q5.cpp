e41bdba565c64e0db63d5a1c632251a3d53a4063
TusharChaudhary-tcc <tusharchaudhary4u08@gmail.com>
2025-12-15 23:45:56 +0530

    Add questionin in C++ in5

--- C++ Questions/Q5.cpp
+++ C++ Questions/Q5.cpp
@@ -3,15 +3,41 @@ using namespace std;
 
 // Q. Write a code to print Decimal to Binary
 
-int DecToBinary(int num){
-    int ans=0; 
-    int p=1;
-    while(num>0){
-        int rem= num%2;
-        num /= 2;
+// int DecToBinary(int num){
+//     int ans=0; 
+//     int p=1;
+//     while(num>0){
+//         int rem= num%2;
+//         num /= 2;
         
-        ans+=(rem*p);
-        p*=10;
+//         ans+=(rem*p);
+//         p*=10;
+//     }
+//     return ans;
+    
+// }
+
+// int main() {
+//     // int num=50;
+//         for(int i=0; i<50; i++){
+//              cout<<DecToBinary(i)<<endl;
+//         }
+// return 0;
+// }
+
+
+// Q. Write a code to print Binary to Decimal
+
+
+int BinaryToDecimal(int binNum){
+    int ans=0; 
+    int pow=1;
+    while(binNum>0){
+        int rem= binNum%2;
+        ans+=(rem*pow);
+        
+        binNum/=10;
+        pow*=2;
     }
     return ans;
     
@@ -19,8 +45,10 @@ int DecToBinary(int num){
 
 int main() {
     // int num=50;
-        for(int i=0; i<50; i++){
-             cout<<DecToBinary(i)<<endl;
-        }
+    
+        // for(int i=0; i<50; i++){
+             cout<<BinaryToDecimal(1010)<<endl;
+        // }
+
 return 0;
 }
