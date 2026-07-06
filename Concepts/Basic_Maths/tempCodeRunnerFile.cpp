1050934bb79a055ef1d630053e3cd9bb0867f456
TusharChaudhary-tcc <tusharchaudhary6160@gmail.com>
2026-07-06 18:19:53 +0530

    Add basic mathematics programs in C++

--- Concepts/Basic_Maths/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-e=true;
-    // for(int i=2; i<num; i++){
-    //     if(num%i==0){
-    //         isPrime=false;
-    //     }
-
-    // }
-    // if(isPrime) cout<<"Prime Number"<<endl;
-    // else cout<<"Not a Prime Number"<<endl;
