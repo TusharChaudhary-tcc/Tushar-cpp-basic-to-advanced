d1b05da5e7e6eebbf6e2b379c8e80e6ba314a744
TusharChaudhary-tcc <tusharchaudhary6160@gmail.com>
2026-09-14 23:57:36 +0530

    Implement union of two sorted arrays using two pointers

---
+++ ADS CLASS WORK/Array_Operations/02_Union_Of_Arrays.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> unionOfArray(vector<int> arr1, vector<int> arr2, int n, int m){
+    int i=0,j=0;
+    vector<int> result;
+    while(i<n && j<m){
+        if(arr1[i]<arr2[j]){
+            result.push_back(arr1[i]);
+            i++;
+        }
+        else if(arr1[i]>arr2[j]){
+            result.push_back(arr2[j]);
+            j++;
+        }
+        else{
+            result.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+    }
+    while(i<n){
+        result.push_back(arr1[i]);
+        i++;
+    }
+    while(j<m){
+        result.push_back(arr2[j]);
+        j++;
+    }
+
+    return result;
+}
+
+int main(){
+    int n,m;
+    cout<<"Size of Array1: ";
+    cin>>n;
+    cout<<"Size of Array2: ";
+    cin>>m;
+    vector<int> arr1(n);
+    vector<int> arr2(m);
+    cout<<"Enter elements of Array1: ";
+    for(int i=0; i<n; i++){
+        cin>>arr1[i];
+    }
+    cout<<"Enter elements of Array2: ";
+    for(int i=0; i<m; i++){
+        cin>>arr2[i];
+    }
+
+    vector<int> result=unionOfArray(arr1, arr2, n, m);
+
+    for(auto a: result){
+        cout<<a<<" ";
+    }
+
+}
